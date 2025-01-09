a257f534805f175c501b5e49d693fb59287e0a9e
rossvincentdiaz12 <32596310+Rvdiaz012@users.noreply.github.com>
2025-01-09 13:45:40 +0800

    Update auth.php

--- auth.php
+++ auth.php
@@ -10,7 +10,7 @@ session_start();
 $apiKey = '7c02368df14952077e3b1747bd3f1372';
 $apiSecret = '33da385e7bc46ee1f41086a1888b332c';
 $scopes = ['read_products', 'write_products'];
-$redirectUri = 'http://shopifytestapp.transactagain.com/transactagain-shopify-test/callback.php';
+$redirectUri = 'https://shopifytestapp.transactagain.com/transactagain-shopify-test/callback.php';
 
 // Step 1: Redirect to Shopify for OAuth
 if (!isset($_GET['shop'])) {
