fbeb446faccd76d28f85d82513fd4ce4c542bcdc
rossvincentdiaz12 <32596310+Rvdiaz012@users.noreply.github.com>
2025-01-09 14:34:11 +0800

    Update callback.php

--- callback.php
+++ callback.php
@@ -7,7 +7,7 @@ use Shopify\Clients\Rest;
 session_start();
 
 // Shopify app credentials
-$apiSecret = '33da385e7bc46ee1f41086a1888b332c';
+$apiSecret = '9ff329c4aaecee791a2f4d8af8c7a03b';
 
 // Step 2: Verify the callback from Shopify
 if (!isset($_GET['shop']) || !isset($_GET['code'])) {
