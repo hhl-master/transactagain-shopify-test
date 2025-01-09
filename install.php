4f3ea46e3b31e8ac98a119a06696e061f81d2301
rossvincentdiaz12 <32596310+Rvdiaz012@users.noreply.github.com>
2025-01-09 14:48:51 +0800

    Update install.php

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "a66a319ef0651ae2c15a42d50706d2f1";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "https://shopifytestapp.transactagain.com/transactagain-shopify-test/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
